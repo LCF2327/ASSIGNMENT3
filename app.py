91df3a079c6cc3e596ac2602d655cfcc477f844a
LCF2327 <c0917813@mylambton.ca>
2024-11-06 23:50:05 -0500

    M

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+# MongoDB connection
+MONGODB_USERNAME = os.getenv('MONGODB_USERNAME')
+MONGODB_PASSWORD = os.getenv('MONGODB_PASSWORD')
+MONGODB_URI = f"mongodb+srv://{MONGODB_USERNAME}:{MONGODB_PASSWORD}@cluster0.jtnz2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+client = MongoClient(MONGODB_URI)
+db = client.shop_db
+products_collection = db.products
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/products')
+def products():
+    products = products_collection.find()
+    return render_template('products.html', products=products)
+
+if __name__ == '__main__':
+    app.run(debug=True)
